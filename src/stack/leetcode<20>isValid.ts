9bb421b7fc77f8eafd4f8ae353838ddc564458c5
dduohereshui <2245675739@qq.com>
2022-07-30 01:08:28 +0800

    feat: leetcode 20

---
+++ src/stack/leetcode<20>isValid.ts
@@ -0,0 +1,35 @@
+// 有效括号
+/**
+ * 输入：s = "()[]{}"
+ * 输出：true
+ *
+ * 输入：s = "(]"
+ * 输出：false
+ *
+ * 输入：s = "([)]"
+ * 输出：false
+ */
+const s1 = "()[]{}";
+const s2 = "(]";
+const s3 = "([)]";
+const isValid = (s: string): boolean => {
+  const stack: string[] = [];
+  const map: Map<string, string> = new Map([
+    ["(", ")"],
+    ["[", "]"],
+    ["{", "}"],
+  ]);
+
+  for (let i = 0; i < s.length; i++) {
+    if (map.has(s[i])) {
+      stack.push(<string>map.get(s[i]));
+    } else {
+      if (stack.pop() !== s[i]) {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
+
+console.log(isValid(s2));
